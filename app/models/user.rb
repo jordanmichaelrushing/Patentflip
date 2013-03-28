ee273d1d96b8fd66e296584659e13121927aebc0
rushingfitness <Jordan@RushingFitness.com>
2013-03-28 01:57:13 -0400

    Fixing en.yml and password error syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
-  has_secure_password
+ 
 
 	before_save { |user| user.email = email.downcase }
 
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, 
   									format: { with: VALID_EMAIL_REGEX },
   	 								uniqueness: { case_sensative: false }
-                    
-  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password                  
+  validates :password, length: { minimum: 6 }
 	validates :password_confirmation, presence: true
 end
