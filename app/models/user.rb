9f39e7f0fb0f0d6baa83d01c4c6e286bc2745441
rushingfitness <Jordan@RushingFitness.com>
2013-03-29 02:02:22 -0400

    sign in dropbox, fix user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,8 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
- 
+  
+  has_secure_password 
 
 	before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
@@ -24,7 +25,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, 
   									format: { with: VALID_EMAIL_REGEX },
   	 								uniqueness: { case_sensative: false }
-  has_secure_password                  
+                   
   validates :password, length: { minimum: 6 }
 	validates :password_confirmation, presence: true
 
