c3fc4d1ed9a55cdddecbfe604eb46b1b9481bf5f
rushingfitness <Jordan@RushingFitness.com>
2013-03-30 10:15:05 -0400

    add home infographic

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,4 @@
 module StaticPagesHelper
+	def user_type
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,34 @@ module UsersHelper
 												#{size}"
 		image_tag(gravatar_url, alt: user.name, class: "gravatar")
 	end
+
+		def patent_sold
+
+		@user = User.new(params[:user])
+		@sold = 0
+	end
+
+	def patent_buy
+
+		@user = User.new(params[:user])
+		@buy = 0
+	end
+
+	def patent_sale
+		
+		@user = User.new(params[:user])
+		@sale = 0
+	end
+
+	def link_sale
+		@link_to_sale = link_to "Number of Patents for sale: #{patent_sale}", root_path, style: 'color: #000000'
+	end
+
+	def link_sold
+		@link_to_sold = link_to "Number of Patents sold: #{patent_sold}", root_path, style: 'color: #000000'
+	end
+
+	def link_buy
+		@link_to_buy = link_to "Number of Patents bought: #{patent_buy}", root_path, style: 'color: #000000'
+	end
 end
