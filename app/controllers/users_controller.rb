cba14ebc7345b5a9f7ee0dbfd109c371219e375b
rushingfitness <Jordan@RushingFitness.com>
2013-04-06 13:55:37 -0400

    fix update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,21 @@
 class UsersController < ApplicationController
   
-	before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
 	before_filter :correct_user, only: [:edit, :update]
 	before_filter :admin_user, only: :destroy
-
+ 
 	def index
 		@users = User.paginate(page: params[:page])
 	end
-
+ 
 	def show
 		@user = User.find(params[:id])
 	end
-
+ 
   def new
   	@user = User.new
   end
-
+ 
   def create
 		@user = User.new(params[:user])
 		if @user.save
@@ -26,44 +26,42 @@ class UsersController < ApplicationController
 			render 'new'
 		end
 	end
-
-	def message
+ 
+ 
+	def edit
 		@user = User.find(params[:id])
 	end
-
-	def edit
-	end
-
+ 
 	def update
-		if @user.update_attributes{params[:user]}
-			flash[:success] = "Profile updated"
+		if @user.update_attributes(params[:user])
 			sign_in @user
+			flash[:success] = "Profile updated"
 			redirect_to @user
 		else
 			render 'edit'
 		end
 	end
-
+ 
 		def destroy
 			User.find(params[:id]).destroy
 			flash[:success] = "User deleted"
 			redirect_to users_path
 		end
-
+ 
 	private
-
+ 
 	def signed_in_user
 		unless signed_in?
 			store_location
 			redirect_to signin_path, notice: "Please sign in."
 		end
 	end
-
+ 
 	def correct_user
 		@user = User.find(params[:id])
 		redirect_to (root_path), error: "Cannot edit others information!" unless current_user?(@user)
 	end
-
+ 
 	def admin_user
 		redirect_to(root_path) unless current_user.admin?
 	end
