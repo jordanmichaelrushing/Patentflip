0b04b1f60ec7076164c0ce15502195cc0f66c015
rushingfitness <Jordan@RushingFitness.com>
2013-04-09 03:39:01 -0400

    nil default user image in CreateGravs

--- db/migrate/20130407202835_create_gravs.rb
+++ db/migrate/20130407202835_create_gravs.rb
@@ -1,7 +1,7 @@
 class CreateGravs < ActiveRecord::Migration
   def change
     create_table :gravs do |t|
-      t.string :content, default: 'assets/coolguy.png'
+      t.string :content
       t.integer :user_id
     end
   end
