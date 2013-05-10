0796adc1b2d74fc92a373fd56d0cbb98bf9d8a8f
rushingfitness <Jordan@RushingFitness.com>
2013-05-10 15:58:22 -0400

    Change attr_accesible to attr_accesible for file names

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,8 +1,8 @@
 class Auction < ActiveRecord::Base
   attr_accessible :category, :cost, :descrip, :img_url, :integer, :lic_or_sell, 
-  :pat_num, :pat_off_desc, :pat_type, :timer_day, :timer_hr, :timer_min, :title
-  attr_accessible :auction_attributes, :vid, :pic1, :pic2, :pic3, :pic4, :pic5, 
-  :vid_file_name, :pic1_file_name, :pic2_file_name, :pic3_file_name, 
+  :pat_num, :pat_off_desc, :pat_type, :timer_day, :timer_hr, :timer_min, :title, 
+  :auction_attributes, :vid, :pic1, :pic2, :pic3, :pic4, :pic5 
+  attr_accessor :vid_file_name, :pic1_file_name, :pic2_file_name, :pic3_file_name, 
   :pic4_file_name, :pic5_file_name
 
 	has_attached_file :pic1, styles: { a: "500x500>" }, default_url: "", 
